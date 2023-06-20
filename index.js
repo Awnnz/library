7520a8f4ec6980d222e80af442ee6af1cba48bf3
Bryan <[200~130257764+Awnnz@users.noreply.github.com>
2023-06-19 21:21:23 -0700

    create simple library book app

---
+++ index.js
@@ -0,0 +1,143 @@
+// Main
+let myLibrary = [];
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+addToLibrary('A walk to Remember', 'Cats');
+
+// Assigning Variables
+const overlay = document.querySelector('.overlay')
+const btn = document.getElementById('btn');
+const cancelBtn = document.querySelector('.cancel-btn');
+const form = document.querySelector('#form');
+const modal = document.querySelector('.modal');
+const removeBtns = document.querySelectorAll('.remove');
+const submit = document.querySelector('.submit');
+
+// Events
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let title = document.getElementById("title");
+  let author = document.getElementById("author");
+  let pages = document.getElementById("pages");
+
+  let newBook = new Book(title.value, author.value, pages.value);
+  console.log(newBook)
+  modal.classList.add('inactive');
+  document.querySelector('.overlay').classList.remove('active');
+
+  const div = document.createElement('div');
+  div.classList.add('card');
+
+  const titleSpan = document.createElement('span');
+  titleSpan.textContent = title.value;
+  div.appendChild(titleSpan);
+  div.appendChild(document.createElement('br'))
+
+  const authorSpan = document.createElement('span');
+  authorSpan.textContent = author.value;
+  div.appendChild(authorSpan);
+  div.appendChild(document.createElement('br'))
+
+  const pagesSpan = document.createElement('span');
+  pagesSpan.textContent = pages.value;
+  div.appendChild(pagesSpan);
+  div.appendChild(document.createElement('br'));
+
+  const readButton = document.createElement('button');
+  checkReadStatus(readButton);
+  readButton.addEventListener('click', (e) => {
+    toggleRead(readButton);
+  })
+  div.appendChild(readButton);
+  div.appendChild(document.createElement('br'));
+
+  const removeButton = document.createElement('button');
+  removeButton.classList.add('remove');
+  removeButton.textContent = 'Remove';
+  div.appendChild(removeButton);
+
+
+  
+
+  document.querySelector('.card-container').appendChild(div);
+  addRemoveCardEventListener(removeButton);
+});
+
+
+btn.addEventListener('click', () => {
+  form.reset();
+  modal.classList.remove('inactive')
+  document.querySelector('.overlay').classList.add('active');
+});
+
+cancelBtn.addEventListener('click', (button) => {
+  modal.classList.add('inactive')
+  document.querySelector('.overlay').classList.remove('active');
+});
+
+overlay.addEventListener('click', () => {
+  modal.classList.add('inactive');
+  document.querySelector('.overlay').classList.remove('active');
+})
+
+removeBtns.forEach(button => {
+  button.addEventListener('click', () => {
+    button.parentElement.addEventListener('transitionend', () => button.parentElement.remove())
+    button.parentElement.style.transform = 'scale(0)';
+  })
+})
+
+// submit.addEventListener('click', (e) => {
+//   e.preventDefault()
+// })
+
+// Functions
+function addToLibrary(...books) {
+  myLibrary.push(...books)
+}
+
+function addRemoveCardEventListener(button) {
+  button.addEventListener('click', () => {
+    button.parentElement.addEventListener('transitionend', () => button.parentElement.remove())
+    button.parentElement.style.transform = 'scale(0)';
+  })
+}
+
+function checkReadStatus(button) {
+  if (document.querySelector('#accept').checked === true) {
+    button.classList.add('read');
+    button.textContent = document.querySelector('#accept').value;
+  }
+  else {
+    button.classList.add('unread');
+    button.textContent = 'Not Read';
+  }
+}
+
+function toggleRead(button) {
+  if (button.classList.contains('read')) {
+    button.classList.replace('read', 'unread');
+    button.textContent = 'Not read';
+  }
+  else {
+    button.classList.replace('unread', 'read');
+    button.textContent = 'Read';
+  }
+}
+
+
+// myLibrary.forEach((book, index) => {
+  
+//   if (index === myLibrary.length -1) document.querySelector('.book-titles').textContent += `${book}.`;
+//   else document.querySelector('.book-titles').textContent += `${book}, `;
+  
+// })
+
+
+
